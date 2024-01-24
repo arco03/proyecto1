358937d1818d4594557b6d14aa7fbeaee1e2e856
arco03 <dmglux13@gmail.com>
2024-01-23 23:13:51 -0500

    Cambios 23/01/24
    Se agrego el prefab de la manzana y se creo el Script para ubicarlo en posiciones random

---
+++ Assets/scripts/posicionRandom.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class posicionRandom : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    public GameObject prefab;
+    public float SpawnChance;
+
+    [Header("Raycast Settings")]
+    public float distanceBetweenCheck;
+    public float heightOfCheck = 10f, rangeOfCheck = 30f;
+    public LayerMask layerMask;
+    public Vector2 positivePosition, negativePosition;
+
+
+    private void Start()
+    {
+        SpawnResources();
+    }
+
+    void SpawnResources()
+    {
+        for (float x = negativePosition.x; x < positivePosition.x; x += distanceBetweenCheck)
+        {
+            for(float z = negativePosition.y; z < positivePosition.y; z += distanceBetweenCheck)
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(new Vector3(x, heightOfCheck,z),Vector3.down, out hit, rangeOfCheck, layerMask))
+                {
+                    if(SpawnChance > Random.Range(0, 101)) 
+                    {
+                        Instantiate(prefab, hit.point, Quaternion.Euler(new Vector3(0, Random.Range(0,360), 0)), transform);   
+                    }
+                }
+            }
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
